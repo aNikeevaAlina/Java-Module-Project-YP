0e6f16d54a2cb042ae7c91e3393e2aa64f89ccbc
Alina <anikeevalinka@yandex.ru>
2023-02-05 18:58:23 +0300

    Проектная работа

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,19 @@
-// dev branch for Y.Practicum
 public class Main {
 
     public static void main(String[] args) {
-        // ваш код начнется здесь
-        // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        System.out.println("Привет Мир");
+
+        BillCalculator billCalculator = new BillCalculator();
+
+        billCalculator.startBillCalculator();
+
     }
+
+}
+
+class Product {
+
+    String name;
+
+    double price;
+
 }
